12c9a7ccbc54f13b3efdcb1c512e111a6e1a294f
gulgulyil <72104981+gulgulyil@users.noreply.github.com>
2024-12-17 14:46:34 +0100

    firstProject

---
+++ src/test/java/day01/firstProject.java
@@ -0,0 +1,15 @@
+package day01;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class firstProject {
+    public static void main(String[] args) {
+        //Webdriver star
+        WebDriver driver= new ChromeDriver();
+
+        //Google ope
+        driver.get("https://www.google.com/");
+
+    }
+}
